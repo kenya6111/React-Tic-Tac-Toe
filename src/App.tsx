0dffa79a0a9b7fcab425ecbc7c583a5f910bf10f
kenya6111 <kenyanke6111@gmail.com>
2025-04-30 01:52:54 +0900

    不要なimport削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState,useEffect } from 'react';
 import './App.css';
 import { Block } from './components/Block';
-import path from 'path';
 
 function App() {
   const [isPlayerX , setIsPlayerX] = useState(true)
